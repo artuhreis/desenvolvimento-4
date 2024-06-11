7181f60085e380db016fbabedd54040afe2b879e
artuhreis <arthu.reis@hotmail.com>
2024-06-11 19:01:13 -0300

    primeiro commit

---
+++ desenvolvimento4.py
@@ -0,0 +1,26 @@
+# Definindo a função da Calculadora
+def calculadora (num1,num2,operacao):
+    # Função a operação.
+    if operacao == 1:
+        return  num1 + num2
+    
+    elif operacao == 2:
+        return num1 - num2
+    
+    elif operacao == 3:
+        return num1 * num2
+    
+    elif operacao == 4:
+        return num1 / num2
+    
+    elif operacao > 4:
+        return 0
+    
+# Coletando os Parametros
+num1 = float(input("Digite o Primeiro Número: "))
+num2 = float(input("Digite o Segundo Número: "))
+operacao = int(input("Escolha a operação (1: Soma, 2: Subtração, 3: Multiplicação, 4: Divisão): "))
+
+# Entregando o Resultado
+resultado = calculadora(num1, num2, operacao)
+print("O Resultado é " , resultado)
